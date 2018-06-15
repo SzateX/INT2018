9b5d6da8bcd831e261de02af6d10601877f2c106
Amy <e.brudzisz@gmail.com>
2018-06-15 20:46:01 +0200

    Templates for news: list and detail

--- INT/urls.py
+++ INT/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
-from .views import HomepageView
+from .views import HomepageView, NewsListView, NewsDetailView
 
 urlpatterns = [
-    url(r"^$", HomepageView.as_view(), name="Homepage")
+    url(r"^$", HomepageView.as_view(), name="Homepage"),
+    url(r"^news$", NewsListView.as_view(), name="NewsList"),
+    url(r"^news/(?P<pk>\d+)$", NewsDetailView.as_view(), name="NewsDetail")
 ]
