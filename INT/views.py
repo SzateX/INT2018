aa629a30fd65705785ca7489195cc2f3dd176a38
Amy <e.brudzisz@gmail.com>
2018-06-15 21:26:26 +0200

    Modified view for speaker details

--- INT/views.py
+++ INT/views.py
@@ -29,7 +29,8 @@ class SpeakerDetailView(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super(SpeakerDetailView, self).get_context_data(**kwargs)
-        context['lectures'] = Lecture.objects.filter(speaker=self.object).order_by('begin_time')
+        ls = SpeakerLecture.objects.filter(speaker_id=self.object)
+        context['lectures'] = ls.select_related('lecture_id')
         return context
 
 
