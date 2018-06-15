c16128e70ac4158477b09c58033f6bff00e470d3
Amy <e.brudzisz@gmail.com>
2018-06-15 22:08:15 +0200

    Added Kodziarze.TK page

--- INT/urls.py
+++ INT/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
-from .views import HomepageView, NewsListView, NewsDetailView, SpeakerDetailView, CompanyDetailView
+from .views import HomepageView, NewsListView, NewsDetailView, SpeakerDetailView, CompanyDetailView, KodziarzeView
 
 urlpatterns = [
     url(r"^$", HomepageView.as_view(), name="Homepage"),
     url(r"^news$", NewsListView.as_view(), name="NewsList"),
     url(r"^news/(?P<pk>\d+)$", NewsDetailView.as_view(), name="NewsDetail"),
     url(r"^speakers/(?P<pk>\d+)$", SpeakerDetailView.as_view(), name="SpeakerDetail"),
-    url(r"^company/(?P<pk>\d+)$", CompanyDetailView.as_view(), name="CompanyDetail")
+    url(r"^company/(?P<pk>\d+)$", CompanyDetailView.as_view(), name="CompanyDetail"),
+    url(r"^kodziarze$", KodziarzeView.as_view(), name="Kodziarze")
 ]
