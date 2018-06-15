b59e0b90f716c7dc9df69b5191feb5b8f82ce6a8
Amy <e.brudzisz@gmail.com>
2018-06-15 21:05:34 +0200

    Template for speaker template, moved navbar to base

--- INT/urls.py
+++ INT/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
-from .views import HomepageView, NewsListView, NewsDetailView
+from .views import HomepageView, NewsListView, NewsDetailView, SpeakerDetailView, CompanyDetailView
 
 urlpatterns = [
     url(r"^$", HomepageView.as_view(), name="Homepage"),
     url(r"^news$", NewsListView.as_view(), name="NewsList"),
-    url(r"^news/(?P<pk>\d+)$", NewsDetailView.as_view(), name="NewsDetail")
+    url(r"^news/(?P<pk>\d+)$", NewsDetailView.as_view(), name="NewsDetail"),
+    url(r"^speakers/(?P<pk>\d+)$", SpeakerDetailView.as_view(), name="SpeakerDetail"),
+    url(r"^company/(?P<pk>\d+)$", CompanyDetailView.as_view(), name="CompanyDetail")
 ]
