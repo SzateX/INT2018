482223e0307e947c2376ea7eaf823944410ad73a
SzateX <jaksza18@gmail.com>
2018-07-17 13:08:37 +0200

    Made setup.py file

---
+++ setup.py
@@ -0,0 +1,19 @@
+import os
+from setuptools import setup, find_packages
+
+setup(
+    name="INT2018",
+    version="0.5.0",
+    author="Jakub Szatkowski",
+    author_email="jaksza18@gmail.com",
+    description=("INT conference service."),
+    install_requires=['django', 'django_extensions', 'martor', 'Pillow', 'factory_boy'],
+    license="MiT",
+    keywords="virtual scoreboard score sport",
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: MiT",
+    ]
+)
