f5e93a23021c2cc19e4c34322960b4be7f857e88
Jakub Szatkowski <mistrzswiata.salata@gmail.com>
2018-08-18 22:04:35 +0200

    corrected

--- INT2018/asgi.py
+++ INT2018/asgi.py
@@ -1,5 +1,5 @@
 import os
 from channels.layers import get_channel_layer
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'basic.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'INT2018.settings')
 application = get_channel_layer()
