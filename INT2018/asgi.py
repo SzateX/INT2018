0bb74592bcc5a279cc1f7321c00eaad9df952817
Jakub Szatkowski <mistrzswiata.salata@gmail.com>
2018-08-18 22:08:11 +0200

    corrected

--- INT2018/asgi.py
+++ INT2018/asgi.py
@@ -1,5 +1,7 @@
 import os
 from channels.layers import get_channel_layer
+import django
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'INT2018.settings')
+django.setup()
 application = get_channel_layer()
